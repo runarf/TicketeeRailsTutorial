1e0eb635423e1fd5c9fa222edb186e028158ade1
Runar Flåten <runarflaaten@gmail.com>
2015-02-02 14:30:11 +0100

    Add the ability to attach a file to a ticket

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150130114448) do
+ActiveRecord::Schema.define(version: 20150202132623) do
+
+  create_table "assets", force: :cascade do |t|
+    t.string   "asset"
+    t.integer  "ticket_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "permissions", force: :cascade do |t|
     t.integer  "user_id"
@@ -36,6 +43,7 @@ ActiveRecord::Schema.define(version: 20150130114448) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "user_id"
+    t.string   "asset"
   end
 
   add_index "tickets", ["project_id"], name: "index_tickets_on_project_id"
