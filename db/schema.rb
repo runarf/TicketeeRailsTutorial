b5c3e00727d888ac17aebd20cb7a135dd4bf049d
Runar Flåten <runarflaaten@gmail.com>
2015-02-05 15:34:59 +0100

    Privatized file agains user without permission, but still use public folder

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150202141326) do
+ActiveRecord::Schema.define(version: 20150205142203) do
 
   create_table "assets", force: :cascade do |t|
     t.string   "asset"
     t.integer  "ticket_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.string   "content_type"
   end
 
   create_table "permissions", force: :cascade do |t|
