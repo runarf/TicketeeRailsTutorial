ec3dc34cf74eeb9e6db47ea70bcf1a07fa271192
Runar Flåten <runarflaaten@gmail.com>
2015-03-19 13:57:21 +0100

    Still working on the javascript bug

--- public/assets/application-97257136b20c03a6c5e2fca689e96f66.js
+++ public/assets/application-97257136b20c03a6c5e2fca689e96f66.js
@@ -10839,18 +10839,6 @@ return jQuery;
 
 }).call(this);
 (function() {
-  $(function() {
-    return $('a#add_another_file').click(function() {
-      var url;
-      url = "/files/new?number=" + $('#files input').length;
-      return $.get(url, function(data) {
-        return $('#files').append(data);
-      });
-    });
-  });
-
-}).call(this);
-(function() {
 
 
 }).call(this);
